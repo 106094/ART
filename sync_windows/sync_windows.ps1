29c1559662903ddcc02f2462d450ded052620825
LazyFinger <97865400+106094@users.noreply.github.com>
2025-11-25 13:22:48 +0800

    Change exclusion pattern in robocopy command

--- sync_windows/sync_windows.ps1
+++ sync_windows/sync_windows.ps1
@@ -4,16 +4,16 @@ $sourcePaths =get-childitem "C:\Users\user\Desktop\ART_*" -directory
 
 $destPath = "\\192.168.50.175\Users\user\Desktop\"
 foreach($sourcePath in $sourcePaths){
-   $sourcefolderpath=$sourcePath.fullname  
+   $sourcefolderpath=$sourcePath.fullname
+     $sourcename=$sourcePath.name
     $destination = Get-ChildItem -path $destPath -Directory | Where-Object { $_.Name -eq  $sourcename }
     if (test-path $destination.FullName) {
      $sourfolders= get-childitem $sourcefolderpath -directory|Where-Object{$_.name -notlike "*old*" -and $_.name -notlike "*temp*"}
-
          foreach ($sourcefolder in $sourfolders){     
          $sourcefoldername=$sourcefolder.name
          $sourcefolderpath=$sourcefolder.fullname
              $destFullPath=join-path  ( $destination.FullName) $sourcefoldername
-            robocopy  $sourcefolderpath $destFullPath /MIR /XD "*data*"
+            robocopy  $sourcefolderpath $destFullPath /MIR /XD "DATA"
          
          }
     }
